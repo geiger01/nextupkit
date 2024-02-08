eca0c16cc96d8e588017ff477df52aa3aec37cb7
geiger01 <tantangeiger@gmail.com>
2024-02-08 22:36:38 +0200

    added providers

---
+++ src/lib/providers.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import React, { ReactNode } from "react";
+import Script from "next/script";
+import { SessionProvider } from "next-auth/react";
+
+const AuthProvider = ({ children }: { children: ReactNode; }) => {
+    return <SessionProvider>{children}</SessionProvider>;
+};
+
+export const CrispChatProvider = () => {
+    const crispId = process.env.NEXT_PUBLIC_CRISP_ID;
+    const isDev = process.env.NODE_ENV === 'development';
+
+    if (!crispId || isDev) {
+        return null;
+    }
+
+    return (
+        <Script
+            id="crisp-widget"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+      window.$crisp=[];
+      window.CRISP_WEBSITE_ID="${crispId}";
+      (function(){
+        const d = document;
+        const s = d.createElement("script");
+        s.src = "https://client.crisp.chat/l.js";
+        s.async = 1;
+        d.getElementsByTagName("head")[0].appendChild(s);
+      })();`,
+            }}
+        />
+    );
+};
+
+export const GoogleAnalyticsProvider = () => {
+    const gtag = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+    const isDev = process.env.NODE_ENV === 'development';
+    
+    if (!gtag || isDev) {
+        return null;
+    }
+
+    return (
+        <>
+            <Script
+                async
+                src={`https://www.googletagmanager.com/gtag/js? 
+      id=${gtag}`}
+            ></Script>
+            <Script
+                id="google-analytics"
+                dangerouslySetInnerHTML={{
+                    __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag}');
+        `,
+                }}
+            ></Script>
+        </>
+    );
+};
+
+
+export const Providers = ({ children }: { children: ReactNode; }) => {
+    return (
+        <>
+            <GoogleAnalyticsProvider />
+            <CrispChatProvider />
+            <AuthProvider>
+                {children}
+            </AuthProvider>
+        </>
+    );
+};
