b721d01bb459e35f63e0af1f11c0fc2deead7015
geiger01 <tantangeiger@gmail.com>
2024-02-09 21:53:15 +0200

    removed unused code

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -52,30 +52,6 @@ export const authOptions: NextAuthOptions = {
 		}),
 	],
 	callbacks: {
-		// async signIn({ user, account }) {
-		// 	// Allow OAuth without email verification
-		// 	if (account?.provider !== 'credentials') return true;
-
-		// 	const existingUser = await getUserById(user.id);
-
-		// 	// Prevent sign in without email verification
-		// 	if (!existingUser?.emailVerified) return false;
-
-		// 	if (existingUser.isTwoFactorEnabled) {
-		// 		const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
-		// 			existingUser.id
-		// 		);
-
-		// 		if (!twoFactorConfirmation) return false;
-
-		// 		// Delete two factor confirmation for next sign in
-		// 		await db.twoFactorConfirmation.delete({
-		// 			where: { id: twoFactorConfirmation.id },
-		// 		});
-		// 	}
-
-		// 	return true;
-		// },
 		async session({ token, session }) {
 			if (session.user) {
 				if (token.sub) {
