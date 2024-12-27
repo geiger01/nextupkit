da59737030f134dd73238e53915630a22e728fdd
geiger01 <tantangeiger@gmail.com>
2024-12-27 17:49:44 +0200

    terms and privacy pages

--- src/app/(legal)/privacy/page.tsx
+++ src/app/(legal)/privacy/page.tsx
@@ -1,16 +1,20 @@
-// import { DashboardHeader } from '@/components/dashboard-header';
 import { Footer } from '@/components/footer';
+import { Header } from '@/components/header';
 
 export const metadata = {
     title: "Privacy Policy",
 };
 
-export default async function Terms() {
-
+export default async function PrivacyPage() {
     return (
         <>
-            {/* <DashboardHeader/> */}
+            <Header />
             <section className='prose dark:prose-invert lg:prose-lg container max-w-5xl py-[50px]'>
+                <h1>Privacy Policy</h1>
+                <p>Last updated: January 1, 2025</p>
+                <p>
+                    Privacy Policy Example: <a className='text-primary' href="https://www.lecturekit.io/privacy-policy" rel="external nofollow noopener" target="_blank">https://www.lecturekit.io/privacy-policy</a>
+                </p>
             </section>
             <Footer />
         </>
--- src/app/(legal)/terms/page.tsx
+++ src/app/(legal)/terms/page.tsx
@@ -1,16 +1,20 @@
-// import { DashboardHeader } from '@/components/dashboard-header';
 import { Footer } from '@/components/footer';
-import Link from 'next/link';
+import { Header } from '@/components/header';
 
 export const metadata = {
     title: "Terms & Conditions",
 };
 
-export default async function Terms() {
+export default async function TermsPage() {
     return (
         <>
-            {/* <DashboardHeader /> */}
+            <Header />
             <section className='prose dark:prose-invert lg:prose-lg container max-w-5xl py-[50px]'>
+                <h1>Terms & Conditions</h1>
+                <p>Last updated: January 1, 2025</p>
+                <p>
+                    Terms & Conditions Example: <a className='text-primary' href="https://www.lecturekit.io/terms-and-conditions" rel="external nofollow noopener" target="_blank">https://www.lecturekit.io/terms-and-conditions</a>
+                </p>
             </section>
             <Footer />
         </>
