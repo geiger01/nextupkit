44e305468e34a76b339843757874dbcfe4ec0f12
geiger01 <tantangeiger@gmail.com>
2024-12-31 18:25:55 +0200

    added catch phrase to footer + NextUpKit built with badge

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,11 +8,18 @@ export const Footer = () => {
                 <Link aria-label="Logo" href="/" className="items-center md:flex">
                     <Logo />
                 </Link>
-                {/* <p className="mt-2 text-center text-muted-foreground">Your Catch Line</p> */}
+                <p className="mt-2 text-center text-muted-foreground">Ship Startups Fast, Without Spending Much</p>
                 <div className="border-b w-full my-5"></div>
 
                 <div className="flex justify-between w-full flex-wrap-reverse gap-5 items-center">
                     <div className="flex flex-col gap-2">
+                        <Link
+                            href="https://www.nextupkit.com/"
+                            target="_blank"
+                            className='w-fit text-xs border rounded-md py-1 px-2 text-muted-foreground flex items-center gap-2'
+                        >
+                            Built with <Logo className='h-[20px] w-[90px]' />
+                        </Link>
                         <p className="text-xs font-light text-muted-foreground">
                             &copy; {new Date().getFullYear()} All rights reserved.
                         </p>
