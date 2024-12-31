4d5d00bb93d283388d7ea9254d2edd6ec7e571a2
geiger01 <tantangeiger@gmail.com>
2024-12-31 18:21:10 +0200

    removed privacy example links

--- src/app/(legal)/privacy/page.tsx
+++ src/app/(legal)/privacy/page.tsx
@@ -12,9 +12,6 @@ export default async function PrivacyPage() {
             <section className='prose dark:prose-invert lg:prose-lg container max-w-5xl py-[50px]'>
                 <h1>Privacy Policy</h1>
                 <p>Last updated: January 1, 2025</p>
-                <p>
-                    Privacy Policy Example: <a className='text-primary' href="https://www.lecturekit.io/privacy-policy" rel="external nofollow noopener" target="_blank">https://www.lecturekit.io/privacy-policy</a>
-                </p>
             </section>
             <Footer />
         </>
--- src/app/(legal)/terms/page.tsx
+++ src/app/(legal)/terms/page.tsx
@@ -12,9 +12,6 @@ export default async function TermsPage() {
             <section className='prose dark:prose-invert lg:prose-lg container max-w-5xl py-[50px]'>
                 <h1>Terms & Conditions</h1>
                 <p>Last updated: January 1, 2025</p>
-                <p>
-                    Terms & Conditions Example: <a className='text-primary' href="https://www.lecturekit.io/terms-and-conditions" rel="external nofollow noopener" target="_blank">https://www.lecturekit.io/terms-and-conditions</a>
-                </p>
             </section>
             <Footer />
         </>
