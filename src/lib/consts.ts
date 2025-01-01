af91858d7b99b612a509dd44e5224968f2572292
geiger01 <tantangeiger@gmail.com>
2025-01-01 23:33:09 +0200

    docs

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -4,4 +4,5 @@ export const FROM_EMAIL = 'noreply@yourdomain.com'; // For your email provider
 export const ROUTES = [
 	{ name: 'Dashboard', path: '/dashboard' },
 	{ name: 'Pricing', path: '/pricing' },
+	{ name: 'Documentation', path: 'https://docs.nextupkit.com' },
 ];
